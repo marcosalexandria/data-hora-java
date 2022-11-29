eb4407b1b3d835158855980de341c530c0e3e742
Marcos Alexandria <marcos.alexandria01@gmail.com>
2022-11-29 10:01:44 -0300

    02

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -19,7 +20,7 @@ public class Main {
 
         DateTimeFormatter form1 = DateTimeFormatter.ofPattern("dd/MM/yyyy"); //estabelecendo um formato
         LocalDate dt5 = LocalDate.parse("01/11/2015", form1); //instanciando com o formato desejado
-        System.out.println(dt5.format(form1)); //imprimindo com mesmo formato
+        //System.out.println(dt5.format(form1)); //imprimindo com mesmo formato
 
         // ----------------------------- pegando elementos --------------------------
 
@@ -28,5 +29,37 @@ public class Main {
         Integer dia = dt6.getDayOfMonth(); //retorna o dia
         Integer mes = dt6.getMonthValue(); //retorna o mes
         Integer ano = dt6.getYear();       //retorna o ano
+
+        // ----------------------------- cálculo com data-hora --------------------------
+
+        LocalDate dt7 = LocalDate.parse("2015-11-01");
+        LocalDateTime dt8 = LocalDateTime.parse("2015-11-01T05:30");
+
+        //os metódos plus adicionam dias, anos, meses, horas... e os metodos minus diminuem
+
+        LocalDate semanaAnteriorLocalDate =  dt7.minusDays(7);
+        //System.out.println("semanaAnteriorLocalDate = " + semanaAnteriorLocalDate);
+        LocalDate proximaSemana = dt7.plusDays(7);
+        //System.out.println("proximaSemana = " + proximaSemana);
+
+        LocalDateTime horasAntesLocalDateTime = dt8.minusHours(3);
+        //System.out.println("horasAntesLocalDateTime = " + horasAntesLocalDateTime);
+        LocalDateTime mesDeposLocalDateTime = dt8.plusMonths(1);
+        //System.out.println("mesDeposLocalDateTime = " + mesDeposLocalDateTime);
+
+        // ----------------------------- Duração entre data-hora --------------------------
+        LocalDateTime dt9 = LocalDateTime.parse("2015-11-01T05:30");
+        LocalDateTime dt10 = LocalDateTime.parse("2015-11-08T05:30");
+        Duration du1 = Duration.between(dt9, dt10); //objeto Duration com o metodo between faz retorna o intervalo entre duas data-hora
+
+        System.out.println("du1 = " + du1.toDays()); //metodo toDays() expecifica que é o intervalo de dias
+
+        //para fazer com LocalDate, ele tem que ser convertido para LocalDateTime
+        LocalDate dt11 = LocalDate.parse("2015-11-01");
+        LocalDate dt12 = LocalDate.parse("2015-11-08");
+        //atStartOfDay = no inicio do dia ou seja colocou essa data as meia noite 00:00:00
+        Duration du2 = Duration.between(dt11.atStartOfDay(), dt12.atStartOfDay());
+        System.out.println("du2 = " + du2.toDays());
+
     }
 }
